95aa3369c63a039bc9d4290191e3d5273065cf51
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-04-13 14:32:35 -0700

    docs
    nothing special, writing some docs (need to update classes to reflect origin in buffer/stream

---
+++ src/structlib/errors.py
@@ -0,0 +1,27 @@
+from collections import Callable
+
+
+class StructError(Exception):
+    ...
+
+
+class UnpackError(StructError):
+    ...
+
+
+class PackError(StructError):
+    ...
+
+
+class ArgTypeError(PackError):
+    ...
+
+
+class UnpackBufferSizeError(UnpackError):
+    def __init__(self, func_name: str, buf_size: int, expected_size: int):
+        self.func_name = func_name
+        self.buf_size = buf_size
+        self.expected_size = expected_size
+
+    def __str__(self):
+        return f"'{self.func_name}' received a buffer with '{self.buf_size}', expected '{self.expected_size}'!"
--- src/structlib/protocols.py
+++ src/structlib/protocols.py
@@ -3,7 +3,14 @@ from typing import Protocol, Tuple, Any, BinaryIO
 
 
 class SizeLike(Protocol):
+    """
+    Exposes the fixed size of a struct
+    """
+
     def _size_(self) -> int:
+        """
+        The fixed size of the struct
+        """
         ...
 
 
@@ -13,7 +20,14 @@ class SizeLikeMixin:
 
 
 class AlignLike(Protocol):
+    """
+    Exposes the alignment of a struct
+    """
+
     def _align_(self) -> int:
+        """
+        The alignment (in bytes) the struct will align to
+        """
         ...
 
 
@@ -23,7 +37,12 @@ class AlignLikeMixin:
 
 
 class ArgLike(Protocol):
+    """
+    The # of fields this structure holds
+    """
+
     def _args_(self) -> int:
+        """ The # of fields this structure expects """
         ...
 
 
@@ -33,6 +52,10 @@ class ArgLikeMixin:
 
 
 class RepLike(Protocol):
+    """
+    The # of times this struct is repeated
+    """
+
     def _reps_(self) -> int:
         ...
 
@@ -43,15 +66,29 @@ class RepLikeMixin:
 
 
 class PackLike(Protocol):
-    def pack(self, *args) -> bytes:
+    """
+    Exposes methods to pack/unpack a structure directly to/from bytes
+    """
+
+    def pack(self, *args: Any) -> bytes:
+        """
+        Packs the structure into it's byte format
+        :param args: The arguments to pack.
+        :raises: PackError: A special error occurred while packing.
+        :raise: ArgTypeError: An argument was not of a valid type.
+        """
         ...
 
     def unpack(self, buffer: bytes) -> Tuple[Any, ...]:
+        """ 
+        Unpacks the structure from it's byte format
+        :raises: UnpackError: A special error occurred while unpacking.
+        """
         ...
 
 
 class PackLikeMixin:
-    def pack(self, *args) -> bytes:
+    def pack(self, *args: Any) -> bytes:
         raise NotImplementedError
 
     def unpack(self, buffer: bytes) -> Tuple[Any, ...]:
@@ -59,18 +96,35 @@ class PackLikeMixin:
 
 
 class BufferPackLike(Protocol):
-    def pack_buffer(self,buffer:bytes,  *args, offset: int = 0) -> int:
+    """
+    Exposes methods to pack/unpack a structure to/from a byte buffer.
+    """
+
+    def pack_buffer(self, buffer: bytes, *args: Any, offset: int = 0, origin: int = 0) -> int:
+        """
+        Pack the arguments into the buffer.
+
+        :param buffer: A byte-like buffer.
+        :param args: The arguments to pack.
+        :param offset: The offset into the buffer, relative to the origin.
+        :param origin: The origin of the struct.
+        :return: The number of bytes written to the buffer.
+        :raises PackError: A special error occurred while packing.
+        :raises ArgTypeError: An argument was not of a valid type.
+        :code:
+
+        """
         ...
 
-    def _unpack_buffer(self, buffer: bytes, *, offset: int = 0) -> Tuple[int, Tuple[Any, ...]]:
+    def _unpack_buffer(self, buffer: bytes, *, offset: int = 0, origin: int = 0) -> Tuple[int, Tuple[Any, ...]]:
         ...
 
-    def unpack_buffer(self, buffer: bytes, *, offset: int = 0) -> Tuple[Any, ...]:
+    def unpack_buffer(self, buffer: bytes, *, offset: int = 0, origin: int = 0) -> Tuple[Any, ...]:
         ...
 
 
 class BufferPackLikeMixin:
-    def pack_buffer(self, buffer:bytes, *args, offset: int = 0) -> int:
+    def pack_buffer(self, buffer: bytes, *args, offset: int = 0) -> int:
         raise NotImplementedError
 
     def _unpack_buffer(self, buffer: bytes, *, offset: int = 0) -> Tuple[int, Tuple[Any, ...]]:
@@ -81,13 +135,13 @@ class BufferPackLikeMixin:
 
 
 class StreamPackLike(Protocol):
-    def pack_stream(self, stream:BinaryIO, *args) -> int:
+    def pack_stream(self, stream: BinaryIO, *args, origin: int = 0) -> int:
         ...
 
-    def _unpack_stream(self, stream: BinaryIO) -> Tuple[int, Tuple[Any, ...]]:
+    def _unpack_stream(self, stream: BinaryIO, origin: int = 0) -> Tuple[int, Tuple[Any, ...]]:
         ...
 
-    def unpack_stream(self, stream: BinaryIO) -> Tuple[Any, ...]:
+    def unpack_stream(self, stream: BinaryIO, origin: int = 0) -> Tuple[Any, ...]:
         ...
 
 
