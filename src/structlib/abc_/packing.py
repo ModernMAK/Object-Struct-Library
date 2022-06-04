3b1bd4bbabe516b3340d44a09dd8def2df88c252
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-06-04 14:59:42 -0800

    Add ConstPackableABC

--- src/structlib/abc_/packing.py
+++ src/structlib/abc_/packing.py
@@ -1,7 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Tuple, BinaryIO, Any
 from structlib.io import bufferio, streamio
-from structlib.protocols.packing import Packable, IterPackable, StructPackable, PrimitivePackable, TPrim, DataclassPackable, DClassType, DClass
+from structlib.protocols.packing import Packable, IterPackable, StructPackable, PrimitivePackable, TPrim, DataclassPackable, DClassType, DClass, ConstPackable
 from structlib.protocols.typedef import align_of, size_of, TypeDefAlignable, TypeDefSizable, T
 from structlib.typing_ import WritableBuffer, ReadableBuffer, ReadableStream, WritableStream
 
@@ -33,6 +33,36 @@ class PackableABC(Packable, TypeDefSizable, TypeDefAlignable, ABC):
         alignment = align_of(self)
         read, packed = streamio.read(stream, size, alignment, origin)
         unpacked = self.unpack(packed)
+        return read, unpacked
+
+
+class ConstPackableABC(ConstPackable, TypeDefSizable, TypeDefAlignable, ABC):
+    """
+    A ConstPackable which uses pack/unpack and a fixed size typedef to perform buffer/stream operations.
+    """
+
+    def const_pack_buffer(self, buffer: WritableBuffer, offset: int, origin: int) -> int:
+        packed = self.const_pack()
+        alignment = align_of(self)
+        return bufferio.write(buffer, packed, alignment, offset, origin)
+
+    def const_unpack_buffer(self, buffer: ReadableBuffer, *, offset: int, origin: int) -> Tuple[int, Any]:
+        size = size_of(self)
+        alignment = align_of(self)
+        read, packed = bufferio.read(buffer, size, alignment, offset, origin)
+        unpacked = self.const_unpack(packed)
+        return read, unpacked
+
+    def const_pack_stream(self, stream: BinaryIO, *args: Any, origin: int) -> int:
+        packed = self.const_pack()
+        alignment = align_of(self)
+        return streamio.write(stream, packed, alignment, origin)
+
+    def const_unpack_stream(self, stream: BinaryIO, *, origin: int) -> Tuple[int, Any]:
+        size = size_of(self)
+        alignment = align_of(self)
+        read, packed = streamio.read(stream, size, alignment, origin)
+        unpacked = self.const_unpack(packed)
         return read, unpacked
 
 
